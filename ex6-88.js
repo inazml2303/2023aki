a620850ec86a2e4674001847d5bc02dd93af1866
inazml2303 <133722862+inazml2303@users.noreply.github.com>
2023-12-12 11:59:04 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,1 +1,1 @@
-function gazo688(){document.getElementById('who688').innerHTML='<img src"./image/nobita.png" alt="オン">';}
+function gazo688(){document.getElementById('who688').innerHTML='<img src"./image/dora2.jpg" alt="オン">';}
