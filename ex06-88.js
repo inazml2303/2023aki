9b4d9ae07dbb4a995046f2275795f7df4f0a4dbf
inazml2303 <133722862+inazml2303@users.noreply.github.com>
2024-01-11 12:39:51 +0900

    Update ex06-88.js

--- ex06-88.js
+++ ex06-88.js
@@ -1,3 +1,3 @@
 function gazo688() {
-    document.getElementById('who688').innerHTML = '<img src="./image/kuruma.png" alt="オン">';
+    document.getElementById('who688').innerHTML = '<img src="./image/kuruma.jpg" alt="オン">';
 }
