3ae404f19b66fac445aca4115d8cbf5153426714
inazml2303 <133722862+inazml2303@users.noreply.github.com>
2024-01-11 12:11:42 +0900

    Update ex06-13.js

--- ex06-13.js
+++ ex06-13.js
@@ -1,4 +1,4 @@
-function mes(){alert("3秒経ちました！");}
+function mes(){alert("３秒経ちました！");}
 function hyoji13(){var str13="一文字ずつ表示します。";
                    var cnt13=document.timer13.moji13.value.length;
                    if(cnt13<11){document.timer13.moji13.value=str13.substr(0,cnt13+1);}else{document.timer13.moji13.value="";}
